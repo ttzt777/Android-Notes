67de881da072c84f3460b9736446fa3af0e1c6eb
TT <tt@thecover.co>
2020-10-23 17:25:27 +0800

    first commit

---
+++ app/src/main/java/cc/bear3/notes/MainActivity.kt
@@ -0,0 +1,18 @@
+package cc.bear3.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cc.bear3.notes.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/cc/bear3/notes/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package cc.bear3.notes.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
